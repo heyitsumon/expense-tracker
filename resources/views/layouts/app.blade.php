732d0d71a87eeafa60922f5dbad35cea3cbbea63
iamsumonmahmud <iamsumonmahmud@gmail.com>
2026-08-21 23:38:33 +0600

    add noties board on top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -522,8 +522,8 @@
 
      <link
         rel="icon"
-        type="image/png"
-        href="{{ asset('favicon.png') }}"
+        type="image/x-icon"
+        href="{{ asset('favicon.ico') }}"
     >
 
 </head>
